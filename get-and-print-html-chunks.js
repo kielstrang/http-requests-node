7a6e6a75bdbb2f6127f0f23a23ff84918d8cb33b
vagrant <kiel.strang@gmail.com>
2017-09-11 18:36:56 +0000

    Step 1

---
+++ get-and-print-html-chunks.js
@@ -0,0 +1,23 @@
+const https = require('https');
+
+function getAndPrintHTMLChunks() {
+
+  const requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, (response) => {
+    response.setEncoding('utf8');
+
+    response.on('data', (data) => {
+      console.log(data);
+    });
+
+    response.on('end', () => {
+      console.log('ended');
+    });
+  });
+}
+
+getAndPrintHTMLChunks();
